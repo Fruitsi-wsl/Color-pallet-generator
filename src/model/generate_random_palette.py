03a7ab3d1e2874652cfd7fc49b14c9d85e0ea88b
Fruitsi <fruitsiii01@gmail.com>
2024-07-24 13:01:55 +0200

    Added script to generate a random palette of 6 colors

---
+++ src/model/generate_random_palette.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import colorsys
+
+palette = []
+
+def generate_color_palette(num_colors):
+
+    for _ in range(num_colors):
+
+        h = np.random.rand()
+
+        s = np.random.uniform(0.5, 1.0)
+
+        v = np.random.uniform(0.5, 1.0)
+
+        r, g, b = colorsys.hsv_to_rgb(h, s, v)
+
+        palette.append((r,g,b))
+    return palette
+
+
+def plot_palette(palette):
+    sns.palplot(palette)
+    plt.show()
+
+
+def create_palette():
+
+    num_colors = 6
+    palette = generate_color_palette(num_colors)
+    plot_palette(palette)
+
+    palette.clear()
+    
+
+create_palette()
