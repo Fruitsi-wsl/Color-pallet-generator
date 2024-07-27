02303179c349290b089010a232c6a9a007f7965b
Fruitsi <fruitsiii01@gmail.com>
2024-07-27 23:57:10 +0200

    Updated generate color coded palette script

--- src/model/generate_color_coded_palette.py
+++ src/model/generate_color_coded_palette.py
@@ -8,9 +8,9 @@ def generate_complementary_palette(initial_color, num_colors):
     # Convert the initial color from RGB to HSV
     r, g, b = initial_color
     h, s, v = colorsys.rgb_to_hsv(r, g, b)
-    
+
     palette = []
-    
+
     # Generate shades and tints of the initial color with some random variation
     for i in range(num_colors // 2):
         factor = i / (num_colors // 2)
@@ -19,10 +19,10 @@ def generate_complementary_palette(initial_color, num_colors):
         new_h = (h + random.uniform(-0.05, 0.05)) % 1.0
         new_r, new_g, new_b = colorsys.hsv_to_rgb(new_h, new_s, new_v)
         palette.append((new_r, new_g, new_b))
-    
+
     # Calculate the complementary hue with some random variation
     complementary_hue = (h + 0.5 + random.uniform(-0.05, 0.05)) % 1.0
-    
+
     # Generate shades and tints of the complementary color with some random variation
     for i in range((num_colors + 1) // 2):  # Ensure we generate enough colors
         factor = i / ((num_colors + 1) // 2)
@@ -31,5 +31,5 @@ def generate_complementary_palette(initial_color, num_colors):
         new_h = (complementary_hue + random.uniform(-0.05, 0.05)) % 1.0
         new_r, new_g, new_b = colorsys.hsv_to_rgb(new_h, new_s, new_v)
         palette.append((new_r, new_g, new_b))
-    
+
     return palette[:num_colors]
